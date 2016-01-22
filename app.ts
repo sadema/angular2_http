242c36fd973be06f24d8e5632c0536d30778a75c
sjoerdadema <sjoerd@kristalsoftware.nl>
2016-01-22 20:40:30 +0100

    remove @Inject

--- app.ts
+++ app.ts
@@ -1,6 +1,5 @@
 import { bootstrap } from "angular2/platform/browser";
 import { Component } from "angular2/core";
-import { Inject } from "angular2/core";
 import { HTTP_PROVIDERS, Http, Response } from "angular2/http";
 
 @Component({
@@ -16,7 +15,7 @@ class HelloWorld {
     data: Object;
     loading: boolean;
     
-    constructor(@Inject(Http) public http: Http) {
+    constructor(public http: Http) {
     }
     
     makeRequest(): void {
@@ -29,33 +28,5 @@ class HelloWorld {
             });
     }
 }
-
-/*
-@Component({
-    selector: 'simple-http',
-    template: `
-        <h2>Basic Request</h2>
-        <button type="button" (click)="makeRequest()">Make request</button>
-        <div *ngIf="loading">loading....</div>
-        <pre>{{data | json}}</pre>
-    `
-})
-class HTTPComponent {
-    data: Object;
-    loading: boolean;
-    
-    constructor(public http: Http) {
-    }
-    
-    makeRequest(): void {
-        this.loading = true;
-        this.http.request('http://jsonplaceholder.typicode.com/posts/1')
-            .subscribe((res: Response) => {
-                this.data = res.json();
-                this.loading = false;
-            });
-    }
-}
-*/
 
 bootstrap(HelloWorld, [ HTTP_PROVIDERS ]);
